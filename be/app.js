b5214c4f4f7b64155a4d65b957a1836be39812cb
Muhammad Amir Mohd Hastapuzi <muhd.amir.m.h@gmail.com>
2025-09-27 14:26:22 +0800

    Feat: Added CORS allowedOrigins to backend

--- be/app.js
+++ be/app.js
@@ -11,6 +11,12 @@ const app = express()
 const port = process.env.PORT || 3000
 const dbURI = process.env.MONGODB_URI
 
+const allowedOrigins = [
+    // 1. Your Vercel domain
+    process.env.FE_URL,
+    process.env.FE_URL_DEV,
+];
+
 mongoose.connect(dbURI)
     .then(() =>
         console.log(`MongoDB connected successfully`),
@@ -23,7 +29,10 @@ mongoose.connect(dbURI)
         process.exit(1); // Exit process with failure
     });
 
-app.use(cors())
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true // Important for handling cookies/JWTs
+}));
 app.use(express.json())
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/chats', chatRoutes)
